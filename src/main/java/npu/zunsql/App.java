6fb4e88f1c6126c890a6e12024ab68231edbfe51
AirYang <yangguang.china@aliyun.com>
2018-10-14 09:57:47 +0800

    select 之后 delete 会崩溃

--- src/main/java/npu/zunsql/App.java
+++ src/main/java/npu/zunsql/App.java
@@ -1,4 +1,5 @@
 package npu.zunsql;
+
 import npu.zunsql.DBInstance;
 import npu.zunsql.cache.CacheMgr;
 import npu.zunsql.cache.Page;
@@ -13,81 +14,64 @@ import java.nio.ByteBuffer;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args)
-    {
-        /* test for readPage and writePage in cacheMgr, when a transation commit
-        CacheMgr cacheManager = new CacheMgr("student");
-        int tranID  = cacheManager.beginTransation("w");
+	public static void main(String[] args) {
+		/*
+		 * test for readPage and writePage in cacheMgr, when a transation commit
+		 * CacheMgr cacheManager = new CacheMgr("student"); int tranID =
+		 * cacheManager.beginTransation("w");
+		 * 
+		 * ByteBuffer tempBuffer = ByteBuffer.allocate(Page.PAGE_SIZE);
+		 * tempBuffer.putInt(0,987); Page tempPage = new Page(tempBuffer);
+		 * cacheManager.writePage(tranID, tempPage); try {
+		 * cacheManager.commitTransation(tranID); } catch (IOException e) {
+		 * e.printStackTrace(); } Page rPage = cacheManager.readPage(2,
+		 * tempPage.getPageID()); int ret = rPage.getPageBuffer().getInt(0);
+		 * System.out.println(ret);
+		 * 
+		 */
 
-        ByteBuffer tempBuffer = ByteBuffer.allocate(Page.PAGE_SIZE);
-        tempBuffer.putInt(0,987);
-        Page tempPage = new Page(tempBuffer);
-        cacheManager.writePage(tranID, tempPage);
-        try
-        {
-            cacheManager.commitTransation(tranID);
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        Page rPage = cacheManager.readPage(2, tempPage.getPageID());
-        int ret = rPage.getPageBuffer().getInt(0);
-        System.out.println(ret);
+		DBInstance dbinstance = DBInstance.Open("test.db");
+		QueryResult result;
+		System.out.println("----create table------------");
+		result = dbinstance.Execute("create table student(stuno int primary key, name varchar,score double)");
 
-        */
+		result = dbinstance.Execute("create table teacher(stuno int primary key, name varchar,score double)");
 
-        DBInstance dbinstance = DBInstance.Open("test.db");
-        QueryResult result;
-        System.out.println("----create table------------");
-        result = dbinstance.Execute("create table student(stuno int primary key, name varchar,score double)");
-        result = dbinstance.Execute("create table teacher(stuno int primary key, name varchar,score double)");
-        
-        //result = dbinstance.Execute("select * from student");
+		// result = dbinstance.Execute("select * from student");
 
-        //result = dbinstance.Execute("insert into student (stuno, name, score) values (2017006, 'zhang', 98.0+1)");
-        result = dbinstance.Execute("insert into teacher (stuno, name, score) values (2017005, 'zhang', 98.0+1)");
-        result = dbinstance.Execute("insert into teacher (stuno, name, score) values (2017004, 'li', 66)");
-        
-//        result = dbinstance.Execute("drop table teacher");
-//        
-//        result = dbinstance.Execute("select * from student");
-//        System.out.println(result.getRes());
-//        
-//        result = dbinstance.Execute("select * from teacher");
-//        System.out.println(result.getRes());
-//        
-//        result = dbinstance.Execute("select * from master");
-//        System.out.println(result.getRes());
-        
-//        result = dbinstance.Execute("update teacher set name='wang' where stuno=2017005");
-      //  result = dbinstance.Execute("select * from teacher");
-       // System.out.println(result.getRes());
-        
-        result = dbinstance.Execute("delete from teacher where stuno=2017004");
-        
-        System.out.println("-----------test update------------");
-        result = dbinstance.Execute("update teacher set name='wang' where stuno=2017005");
-        result = dbinstance.Execute("update teacher set score=666 where stuno=2017005");
-        //System.out.println("---------------update end----------");
-        result = dbinstance.Execute("select * from teacher");	
-        System.out.println(result.getRes());
-        System.out.println("--------update result----------");
-           
-        
-        result = dbinstance.Execute("drop table student");
-        System.out.println(result.getRes());
-        result = dbinstance.Execute("select * from master");
-        System.out.println(result.getRes());        
-        result = dbinstance.Execute("drop table teacher");
+		// result = dbinstance.Execute("insert into student (stuno, name, score) values
+		// (2017006, 'zhang', 98.0+1)");
+		result = dbinstance.Execute("insert into teacher (stuno, name, score) values (2017005, 'zhang', 98.0+1)");
+		result = dbinstance.Execute("insert into teacher (stuno, name, score) values (2017004, 'li', 66)");
+
+		result = dbinstance.Execute("insert into student (stuno, name, score) values (2017005, 'zhao', 60)");
+		
+		result = dbinstance.Execute("select * from teacher");
+		System.out.println(result.getRes());
+
+		result = dbinstance.Execute("delete from teacher where stuno=2017004");
+
+		System.out.println("-----------test update------------");
+		result = dbinstance.Execute("update teacher set name='wang' where stuno=2017005");
+		result = dbinstance.Execute("update teacher set score=666 where stuno=2017005");
+		// System.out.println("---------------update end----------");
+		result = dbinstance.Execute("select * from teacher");
+		System.out.println(result.getRes());
+		System.out.println("--------update result----------");
+
+		result = dbinstance.Execute("drop table student");
+		System.out.println(result.getRes());
+		result = dbinstance.Execute("select * from master");
+		System.out.println(result.getRes());
+		result = dbinstance.Execute("drop table teacher");
 //      System.out.println(result.getRes());
-      
-        result = dbinstance.Execute("select * from master");
-        System.out.println(result.getRes());
 
-        //System.out.println("Insert Row:" + result.getAffectedCount());
-        //result=dbinstance.Execute("select * from student");
+		result = dbinstance.Execute("select * from master");
+		System.out.println(result.getRes());
+
+		// System.out.println("Insert Row:" + result.getAffectedCount());
+		// result=dbinstance.Execute("select * from student");
 //        System.out.println("select Row:"+result.getAffectedCount());
-        dbinstance.Close();
-    }
+		dbinstance.Close();
+	}
 }
